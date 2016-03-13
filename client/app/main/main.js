c1cfcc2c3d47f979660fcd11ef264efbfe8d3e5c
alexabbott <alex.r.abbott@gmail.com>
2016-03-13 19:48:15 -0400

    Add search functionality, modals and cleaned up styling

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -10,7 +10,7 @@ angular.module('meanAppApp')
       });
   });
 
-  var thisSound;
+var thisSound;
 function addURL(test){
   thisSound = test;
   $('.src').attr('value',test);
@@ -97,6 +97,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 90 ) { //z
       function play1(){
            var audio1 = $('.sample-player')[12];
+          $(audio1).parent().addClass('selected');
            if (down['90'] == null) {
               audio1.pause();
               audio1.currentTime = 0;
@@ -109,6 +110,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 88 ) { //x
       function play2(){
            var audio2 = $('.sample-player')[13];
+           $(audio2).parent().addClass('selected');
           if (down['88'] == null) {
             audio2.pause();
             audio2.currentTime = 0;
@@ -121,6 +123,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 67 ) { //c
       function play3(){
            var audio3 = $('.sample-player')[14];
+           $(audio3).parent().addClass('selected');
            if (down['67'] == null) {
              audio3.pause();
              audio3.currentTime = 0;
@@ -133,6 +136,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 86 ) { //v
       function play4(){
            var audio4 = $('.sample-player')[15];
+           $(audio4).parent().addClass('selected');
            if (down['86'] == null) {
              audio4.pause();
              audio4.currentTime = 0;
@@ -145,6 +149,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 83 ) { //s
       function play5(){
            var audio5 = $('.sample-player')[8];
+           $(audio5).parent().addClass('selected');
            if (down['83'] == null) {
              audio5.pause();
              audio5.currentTime = 0;
@@ -157,6 +162,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 68 ) { //d
       function play6(){
            var audio6 = $('.sample-player')[9];
+           $(audio6).parent().addClass('selected');
            if (down['68'] == null) {
              audio6.pause();
              audio6.currentTime = 0;
@@ -169,6 +175,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 70 ) { //f
       function play7(){
            var audio7 = $('.sample-player')[10];
+           $(audio7).parent().addClass('selected');
            if (down['70'] == null) {
              audio7.pause();
              audio7.currentTime = 0;
@@ -181,6 +188,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 71 ) { //g
       function play8(){
            var audio8 = $('.sample-player')[11];
+           $(audio8).parent().addClass('selected');
            if (down['71'] == null) {
              audio8.pause();
              audio8.currentTime = 0;
@@ -193,6 +201,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 69 ) { //e
       function play9(){
            var audio9 = $('.sample-player')[4];
+           $(audio9).parent().addClass('selected');
            if (down['69'] == null) {
              audio9.pause();
              audio9.currentTime = 0;
@@ -205,6 +214,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 82 ) { //r
       function play10(){
            var audio10 = $('.sample-player')[5];
+           $(audio10).parent().addClass('selected');
            if (down['82'] == null) {
              audio10.pause();
              audio10.currentTime = 0;
@@ -217,6 +227,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 84 ) { //t
       function play11(){
            var audio11 = $('.sample-player')[6];
+           $(audio11).parent().addClass('selected');
            if (down['84'] == null) {
              audio11.pause();
              audio11.currentTime = 0;
@@ -229,6 +240,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 89 ) { //y
       function play12(){
            var audio12 = $('.sample-player')[7];
+           $(audio12).parent().addClass('selected');
            if (down['89'] == null) {
              audio12.pause();
              audio12.currentTime = 0;
@@ -241,6 +253,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 52 ) { //4
       function play13(){
            var audio13 = $('.sample-player')[0];
+           $(audio13).parent().addClass('selected');
            if (down['52'] == null) {
              audio13.pause();
              audio13.currentTime = 0;
@@ -253,6 +266,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 53 ) { //5
       function play14(){
            var audio14 = $('.sample-player')[1];
+           $(audio14).parent().addClass('selected');
            if (down['53'] == null) {
              audio14.pause();
              audio14.currentTime = 0;
@@ -265,6 +279,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 54 ) { //6
       function play15(){
            var audio15 = $('.sample-player')[2];
+           $(audio15).parent().addClass('selected');
            if (down['54'] == null) {
              audio15.pause();
              audio15.currentTime = 0;
@@ -277,6 +292,7 @@ $(document).keydown(function(e) {
   if ( e.keyCode === 55 ) { //7
       function play16(){
            var audio16 = $('.sample-player')[3];
+           $(audio16).parent().addClass('selected');
            if (down['55'] == null) {
              audio16.pause();
              audio16.currentTime = 0;
@@ -294,6 +310,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 90 ) { //z
       function pause1(){
            var audio1 = $('.sample-player')[12];
+           $(audio1).parent().removeClass('selected');
             audio1.pause();
             down['90'] = null;
       };
@@ -302,6 +319,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 88 ) { //x
       function pause2(){
            var audio2 = $('.sample-player')[13];
+           $(audio2).parent().removeClass('selected');
             audio2.pause();
             down['88'] = null;
       };
@@ -310,6 +328,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 67 ) { //c
       function pause3(){
            var audio3 = $('.sample-player')[14];
+           $(audio3).parent().removeClass('selected');
            audio3.pause();
            down['67'] = null;
       };
@@ -318,6 +337,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 86 ) { //v
       function pause4(){
            var audio4 = $('.sample-player')[15];
+           $(audio4).parent().removeClass('selected');
            audio4.pause();
            down['86'] = null;
       };
@@ -326,6 +346,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 83 ) { //s
       function pause5(){
            var audio5 = $('.sample-player')[8];
+           $(audio5).parent().removeClass('selected');
            audio5.pause();
            down['83'] = null;
       };
@@ -334,6 +355,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 68 ) { //d
       function pause6(){
            var audio6 = $('.sample-player')[9];
+           $(audio6).parent().removeClass('selected');
            audio6.pause();
            down['68'] = null;
       };
@@ -342,6 +364,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 70 ) { //f
       function pause7(){
            var audio7 = $('.sample-player')[10];
+           $(audio7).parent().removeClass('selected');
            audio7.pause();
            down['70'] = null;
       };
@@ -350,6 +373,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 71 ) { //g
       function pause8(){
            var audio8 = $('.sample-player')[11];
+           $(audio8).parent().removeClass('selected');
            audio8.pause();
            down['71'] = null;
       };
@@ -358,6 +382,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 69 ) { //e
       function pause9(){
            var audio9 = $('.sample-player')[4];
+           $(audio9).parent().removeClass('selected');
            audio9.pause();
            down['69'] = null;
       };
@@ -366,6 +391,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 82 ) { //r
       function pause10(){
            var audio10 = $('.sample-player')[5];
+           $(audio10).parent().removeClass('selected');
            audio10.pause();
            down['82'] = null;
       };
@@ -374,6 +400,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 84 ) { //t
       function pause11(){
            var audio11 = $('.sample-player')[6];
+           $(audio11).parent().removeClass('selected');
            audio11.pause();
            down['84'] = null;
       };
@@ -382,6 +409,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 89 ) { //y
       function pause12(){
            var audio12 = $('.sample-player')[7];
+           $(audio12).parent().removeClass('selected');
            audio12.pause();
            down['89'] = null;
       };
@@ -390,6 +418,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 52 ) { //4
       function pause13(){
            var audio13 = $('.sample-player')[0];
+           $(audio13).parent().removeClass('selected');
            audio13.pause();
            down['52'] = null;
       };
@@ -398,6 +427,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 53 ) { //5
       function pause14(){
            var audio14 = $('.sample-player')[1];
+           $(audio14).parent().removeClass('selected');
            audio14.pause();
            down['53'] = null;
       };
@@ -406,6 +436,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 54 ) { //6
       function pause15(){
            var audio15 = $('.sample-player')[2];
+           $(audio15).parent().removeClass('selected');
            audio15.pause();
            down['54'] = null;
       };
@@ -414,6 +445,7 @@ $(document).keyup(function(e) {
   if ( e.keyCode === 55 ) { //7
       function pause16(){
            var audio16 = $('.sample-player')[3];
+           $(audio16).parent().removeClass('selected');
            audio16.pause();
            down['55'] = null;
       };
